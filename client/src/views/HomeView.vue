32944f9d132a14e50dcf57ae039a1af6a3aef1e7
LL-Etiane <lukongleinyuyetiane@gmail.com>
2024-12-15 06:42:30 +0100

    adds tailwind css to client

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-center font-bold text-5xl">Bridge Africa Tasks</h1>
   </main>
 </template>
